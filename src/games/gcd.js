a39d0ba3867464832e15b33fd57ae2066300bb04
Dmytro Korzhov <dmytro.korzhov@gmail.com>
2017-08-12 18:57:15 +0300

    task 6: add brain-gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,17 @@
+import { cons } from 'hexlet-pairs'; // eslint-disable-line
+import play from '..';
+import { getRandomInt, gcd } from '../util';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const setGameData = () => {
+  const number1 = getRandomInt();
+  const number2 = getRandomInt();
+
+  const question = `${number1} ${number2}`;
+  const correctAnswer = `${gcd(number1, number2)}`;
+
+  return cons(description, cons(question, correctAnswer));
+};
+
+export default () => play(setGameData);
